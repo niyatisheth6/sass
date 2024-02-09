30cb888f51d0a0a7612e3b2e24f31fcc76ea2379
Niyati sheth <niyati.radicalloop@gmail.com>
2024-02-09 18:04:34 +0530

    init

---
+++ react-bootstrap-sass/src/app/App.js
@@ -0,0 +1,20 @@
+import About from "../components/About";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Values from "../components/Values";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <About />
+      <Values />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
